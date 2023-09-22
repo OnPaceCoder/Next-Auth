353529246830db08c61bcc0e598c75bb3bccaa5a
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-09-22 23:07:31 +0530

    Add: Update Password API

---
+++ src/app/api/users/updatepassword/route.ts
@@ -0,0 +1,36 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { NextResponse, NextRequest } from "next/server";
+import bcryptjs from "bcryptjs";
+import User from "@/models/userModel";
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+
+    const { token, newPassword } = reqBody.user;
+    console.log(token);
+    const user = await User.findOne({
+      forgotPasswordToken: token,
+      forgotPasswordTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Invalid token " }, { status: 400 });
+    }
+
+    console.log(user);
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(newPassword, salt);
+
+    user.password = hashedPassword;
+    user.forgotPasswordToken = undefined;
+    user.forgotPasswordTokenExpiry = undefined;
+
+    await user.save();
+
+    return NextResponse.json({ message: "Password Updated ", success: true });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
