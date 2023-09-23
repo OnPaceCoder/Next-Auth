bdb0e9b365abddb79b97b17ccc60e0485cb74927
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-09-23 14:47:50 +0530

    fix: UI

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -21,34 +21,38 @@ const page = () => {
   const getUserDetails = async () => {
     const res = await axios.get("/api/users/me");
     console.log(res.data.data);
-    setData(res.data.data?._id);
+    setData(res.data.data?.username);
   };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-y-2">
-      <h1>Profile</h1>
-      <hr />
-      <p>Profile Page</p>
-      <h2 className="p-2 rounded bg-green-200">
-        {data === "none" ? (
-          "None"
-        ) : (
-          <Link href={`/profile/${data}`}>{data}</Link>
-        )}
-      </h2>
-      <hr />
-      <button
-        onClick={logout}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Logout
-      </button>
-      <button
-        onClick={getUserDetails}
-        className="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Get Data
-      </button>
+      <div className="flex flex-col gap-2 border border-gray-500 p-3 rounded-md w-1/4">
+        <h1 className="text-slate-400 font-bold py-2 text-center text-xl">
+          Profile
+        </h1>
+        <hr />
+        <p>Profile Page</p>
+        <h2 className="p-2 rounded bg-gray-500">
+          {data === "none" ? (
+            "None"
+          ) : (
+            <Link href={`/profile/${data}`}>Welcome {data}</Link>
+          )}
+        </h2>
+        <hr />
+        <button
+          onClick={logout}
+          className="bg-slate-700 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded"
+        >
+          Logout
+        </button>
+        <button
+          onClick={getUserDetails}
+          className="bg-neutral-300 hover:bg-neutral-500 text-white font-bold py-2 px-4 rounded"
+        >
+          Get Data
+        </button>
+      </div>
     </div>
   );
 };
