5804b40e081bb4876ecabd0eb9e3628b027e34c2
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-09-23 14:52:44 +0530

    fix: Function name

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-const page = () => {
+const Page = () => {
   const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
